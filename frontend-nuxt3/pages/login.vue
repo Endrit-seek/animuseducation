74f9e9d50267ba6a13d592064eba30f700f19c11
Endrit Krasniqi <esl.endritkrasniqi@gmail.com>
2023-06-29 23:12:07 +0200

    feat: implemented authentication with laravel sanctum and composables in nuxt

--- frontend-nuxt3/pages/login.vue
+++ frontend-nuxt3/pages/login.vue
@@ -1,9 +1,37 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const form = ref({
+  email: "test@example.com",
+  password: "password"
+})
+
+async function handleLogin() {
+  await useApiFetch("/sanctum/csrf-cookie")
+
+  await useApiFetch("/login", {
+    method: "POST",
+    body: form.value
+  })
+
+  const { data } = await useApiFetch("/api/user")
+
+  console.log(data);
+  
+}
+</script>
 
 <template>
-  <div>
-    Page: foo
-  </div>
+  <form @submit.prevent="handleLogin">
+    <label>
+      Email
+      <input type="email" v-model="form.email">
+    </label>
+    <label>
+      Password
+      <input type="password" v-model="form.password">
+    </label>
+
+    <button>Login</button>
+  </form>
 </template>
 
 <style scoped></style>

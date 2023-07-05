8b1f71107556460f10c1ae29c84de3ecd0e387e1
Endrit Krasniqi <esl.endritkrasniqi@gmail.com>
2023-07-05 22:38:43 +0200

    feat: updated logout method and the middlewares of routes

--- backend-laravel/app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ backend-laravel/app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -7,19 +7,20 @@ use App\Http\Requests\Auth\LoginRequest;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\JsonResponse;
 
 class AuthenticatedSessionController extends Controller
 {
     /**
      * Handle an incoming authentication request.
      */
-    public function store(LoginRequest $request): Response
+    public function store(LoginRequest $request): JsonResponse
     {
         $request->authenticate();
 
-        $request->session()->regenerate();
+        $token = $request->user()->createToken($request->userAgent());
 
-        return response()->noContent();
+        return response()->json(['token' => $token->plainTextToken]);
     }
 
     /**
@@ -27,11 +28,11 @@ class AuthenticatedSessionController extends Controller
      */
     public function destroy(Request $request): Response
     {
-        Auth::guard('web')->logout();
-
-        $request->session()->invalidate();
-
-        $request->session()->regenerateToken();
+        if(method_exists($request->user()->currentAccessToken(), 'delete')) {
+            $request->user()->currentAccessToken()->delete();
+        }
+        
+        auth()->guard('web')->logout();
 
         return response()->noContent();
     }
